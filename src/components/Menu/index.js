e6fc507bcd6649c9affa687cb53d2c2de88d9fd4
uugnaa <uugnaa@gmail.com>
2022-06-22 12:30:55 +0800

    css module

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,10 @@
 import React from "react";
+import css from "./style.module.css"
 
 function Menubar() {
     return ( 
-    <div className="nav">
-    <div className="na">
-  
-    </div>
+    <div className={css.nav}>
+    lorem
         
     </div>
     );
